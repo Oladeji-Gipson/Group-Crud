8a9cbedac46e8f683b7bd193bbd82f5748699748
Oladeji-Gipson <89504900+Oladeji-Gipson@users.noreply.github.com>
2022-02-28 11:35:31 -0600

    LB2

---
+++ LB2/StoreInventory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB2
+{
+    public class StoreInventory
+    {
+        private string _name;
+        private string _upc;
+        private decimal _price;
+        private decimal _cost;
+        private int _units;
+        private string _distributor;
+        private int _position;
+
+
+        public void SetStoreInventory(string name, string upc, decimal price, decimal cost, int units, string distributor, int position)
+        {
+            _name = name;
+            _upc = upc;
+            _price = price;
+            _cost = cost;
+            _units = units;
+            _distributor = distributor;
+            _position = position;
+        }
+
+        public void SetNewPrice(decimal newPrice)
+        {
+            _price = newPrice;
+        }
+
+        public string GetName()
+        {
+            if (_name == null) { _name = ""; }
+            return _name;
+        }
+
+        public string GetUpc()
+        {
+            return _upc;
+        }
+
+        public decimal GetPrice()
+        {
+            return _price;
+        }
+
+        public decimal GetCost()
+        {
+            return _cost;
+        }
+
+        public int GetUnits()
+        {
+            return _units;
+        }
+
+        public string GetDistributor()
+        {
+            return _distributor;
+        }
+
+        public int GetPosition()
+        {
+            return _position;
+        }
+    }
+}
